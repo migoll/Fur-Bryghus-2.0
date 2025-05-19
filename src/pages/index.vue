6aa499700456888338b4820b1f9784f27d30ccb5
Gabbana <chris.lund02@gmail.com>
2025-05-19 08:32:06 +0200

    a

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,20 +3,3 @@
     <h1 class="font-anton">Fur Bryghus – Nu kører det!</h1>
   </div>
 </template>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Anton:wght@400;700&family=Inter:wght@400;700&display=swap');
-
-h1, h2, h3 {
-  font-family: 'Anton', sans-serif;
-}
-
-body, html {
-  font-family: 'Inter', sans-serif;
-}
-
-/* ...existing Tailwind directives... */
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-</style>

55ccbe4e3f6b278620d74f81a70ac80c6003091e
Amalie <amalie.andreasen99@hotmail.com>
2025-05-27 13:03:40 +0200

    selskabsmenu er opsat med de første 2 tabs

---
+++ src/components/tabs/Buffet.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <img src="@/assets/images/desktop/selskabsmenu-banner.webp" alt="restaurant" class="h-[15rem] w-full object-cover ">
+    </div>
+    <div class="flex flex-col items-center content-center gap-4 py-[2rem] ">
+        <h2 class="text-center">Buffet</h2>
+        <p>Kontakt os for at sammensætte din egen menu.</p>
+         <p class="p-medium p-[1rem] font-bold text-center">Klassisk buffet</p>
+    </div>
+    <div class="flex  content-center align-center justify-center">
+    <div  class="flex flex-col md:flex-row w-[70%]">
+        <div>
+            <div class="p-[1rem] mb-[1rem]">
+                <div class="flex content-center items-center justify-between">
+                     <p class="p-medium">Forretter</p>
+                </div>
+                <p>Varmrøget torskesalat med årstidens sprøde salater - urte olie - grøn mayo - urtesalat</p>
+                <p>Confiteret kylling. Serveres med Karl Johan og trøffel creme - syltet svampe - sprød cruiton - tørsaltet bacon - urter</p>
+                <p>Kold røget laks med purløgsmayo - æble relish - malt crumble - marineret urter</p>
+                <p>Til forretter serveres hjemmebagt surdejsbrød, smør og tapenade</p>
+            </div>
+            <div class="p-[1rem] mb-[1rem]">
+                <div class="flex content-center items-center justify-between">
+                     <p class="p-medium">Varme retter</p>
+                </div>
+                <p>Kroket af braiseret gris - humus - bagte urter</p>
+                <p>Vol au vent - skaldyrsragout - urtesalat</p>
+                 <p>Glaceret svinekæbe - grillet hjertesalat - solbær</p>
+                  <p>grillet lam - sæsonens grøntsager  - knust kartoffel</p>
+            </div>
+             <div class="p-[1rem] mb-[1rem]">
+                <div class="flex content-center items-center justify-between">
+                     <p class="p-medium">Desserter</p>
+                </div>
+                <p>Grubelagret Vesterhavsost - Halkidiki oliven - chutney</p>
+                <p>Citron mazzerin tærte - lemoncurd - Brændt marengs</p>
+                <p>Cheesecake - Barleywine gele</p>
+            </div>
+            <p class="font-bold p-[1rem]">Pr. person kr. 625,-</p>
+        </div>
+    </div>
+</div>
+
+<p class="p-medium p-[1rem] font-bold text-center">Tapas buffet</p>
+<div class="flex  content-center align-center justify-center">
+    <div  class="flex flex-col md:flex-row w-[70%]">
+        <div>
+            <div class="p-[1rem] mb-[1rem]">
+                <div class="flex content-center items-center justify-between">
+                     <p class="p-medium">Forretter</p>
+                </div>
+                <p>blinis - rejesalat - citron mayo - karse</p>
+                <p>Hjemmerøget laks - purløgsmayo - æble relish - Syrlig creme</p>
+                <p>grillede tigerrejer - aioli - tangsalat</p>
+                <p>Unghanesalat - mørdej - svampemayo - sprød bacon - urter</p>
+                <p>12 mdr. Bryghus skinke - trøffel mayo - mandler i brunet smør</p>
+            </div>
+            <div class="p-[1rem] mb-[1rem]">
+                <div class="flex content-center items-center justify-between">
+                     <p class="p-medium">Hovedretter</p>
+                </div>
+                <p>Urtemarineret grisefilet. Hertil årstidens grillet/bagt grønt  - perlebyg risotto - 2 slags årstidens salat</p>
+                <p>Farseret perlehønebryst med stegte svampe - dijonsauce - ristet petit kartofler</p>
+            </div>
+             <div class="p-[1rem] mb-[1rem]">
+                <div class="flex content-center items-center justify-between">
+                     <p class="p-medium">Desserter</p>
+                </div>
+                <p>Ostebræt fra Thise Mejeri med behørigt tilbehør</p>
+                <p>Citron mazzerin tærte med brændt marenges og chokolademousse med appelsin-ganache</p>
+            </div>
+            <p class="font-bold p-[1rem]">Pr. person kr. 625,-</p>
+        </div>
+    </div>
+</div>
+
+
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- src/pages/selskaber/selskabsmenuer.vue
+++ src/pages/selskaber/selskabsmenuer.vue
@@ -1,1 +1,33 @@
-<template><div class="p-8 text-xl">Test</div></template>
+<template>
+  <!-- Menukort siden hvor der bruges 3 komponenter til at vise indhold. en herosektion, en intro sektion og menutabs hvor man kan se menukortet i tabs -->
+  <Hero  
+    heroHeading="Selskabsmenuer"
+    :heroImageMobile="heroImageMobile"
+    :heroImageDesktop="heroImageDesktop"
+    heroImageAlt="Billede af maden i restauranten"
+  />
+
+<BasicSection
+heading="Sammen skaber vi en oplevelse"
+    text="Vi har dejlige lokaler til små og store selskaber – og er klar med råd og vejledning om alt fra menu til opdækning og musik. Du er altid velkommen til at ringe til os på 9759 3060 og få en snak med vores Restaurationschef om mulighederne."
+    :imageMobile="mobileImg"
+    :imageDesktop="desktopImg"
+    imageAlt="Restaurant billede"
+    headingColor="text-neutral-1"
+    textColor="text-neutral-1"
+    buttonLabel="Book dit selskab"
+    buttonLink="/book-dit-selskab"
+/>
+
+<SelskabsTabs />
+
+</template>
+
+<script setup>
+import heroImageMobile from '@/assets/images/desktop/selskabsmenuer-hero.webp'
+import heroImageDesktop from '@/assets/images/desktop/selskabsmenuer-hero.webp'
+import mobileImg from '@/assets/images/desktop/selskabsmenu-ret-eksempel.webp'
+import desktopImg from '@/assets/images/desktop/selskabsmenu-ret-eksempel.webp'
+</script>
+
+<style scoped></style>

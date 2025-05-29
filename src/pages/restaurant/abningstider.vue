43a2c464e2937cd4c6aa68e7b62fe2965a2a8826
Gabbana <chris.lund02@gmail.com>
2025-05-29 10:49:21 +0200

    åbningstider added

--- src/pages/restaurant/abningstider.vue
+++ src/pages/restaurant/abningstider.vue
@@ -1,12 +1,12 @@
 <template>
   <Hero
-    heading="Nyheder"
+    heading="Åbningstider"
     :imageMobile="mobileImg"
     :imageDesktop="desktopImg"
     imageAlt="Bryggeriet"
   />
 
-  <section class="flex flex-row items-center mx-auto p-10">
+  <section class="flex flex-col items-center mx-auto p-10 md:flex-row gap-6">
     <div class="flex-1 justify-center">
       <h2>Restaurantens åbningstider</h2>
       <p>
@@ -18,7 +18,26 @@
 
     <div class="flex-1"><Abningstider></Abningstider></div>
   </section>
-  <section class="bg-accent-fur-beer"></section>
+  <section
+    class="bg-fur-accent-beer flex flex-col items-center mx-auto p-10 md:flex-row"
+  >
+    <div class="flex-1 justify-center">
+      <h3>Prøv smagen af fur</h3>
+      <p>
+        Besøg os og få en oplevelse og smagen af Fur. Vi glæder os til at byde
+        dig velkommen. Husk at booke bord på restauranten for at sikre dig en
+        plads.
+      </p>
+      <Button label="Book bord" to="/restaurant/bestil-bord" />
+    </div>
+    <div class="flex-1">
+      <img
+        src="assets\images\desktop\abningstider-smagen-af-fur.webp"
+        alt="Billede af knudeklinten på Fur"
+        class="w-full md:h-full object-cover h-[300px]"
+      />
+    </div>
+  </section>
 </template>
 
 <script setup>

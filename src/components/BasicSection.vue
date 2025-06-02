32a37eda7f337d4c5b4cf682ab053751189706f8
Gabbana <chris.lund02@gmail.com>
2025-06-02 14:24:26 +0200

    fixed comment

--- src/components/BasicSection.vue
+++ src/components/BasicSection.vue
@@ -3,13 +3,15 @@
   <section
     :class="[
       // Hvis der er et baggrundsbillede tilføjes ingen klasse fordi billedet bruges nede i style. hvis der er en faarve istedet bruges baggrundsfarve som klasse, så vi kan sætte den med tailwind css
+      // herunder defineres hvordan baggrundsbilledet sættes hvis der findes det
+
       'px-[1rem] py-[2rem] md:py-[4rem] md:px-[2rem] flex items-center justify-center',
       bgImage ? '' : bgColor,
     ]"
-    // her defineres hvordan baggrundsbilledet sættes hvis der findes det
     :style="
       bgImage
-        ? `background-image: url(${bgImage}); background-size: cover; background-position: center;`
+        ? `background-image: url(${bgImage}); background-size:
+    cover; background-position: center;`
         : ''
     "
   >

c64f293a7c5a9445c54d724645748996e5b979cf
Gabbana <chris.lund02@gmail.com>
2025-06-04 08:33:07 +0200

    fixed import and end tag

--- src/components/OlKarrusel.vue
+++ src/components/OlKarrusel.vue
@@ -3,12 +3,12 @@
   <!-- Her laves en ølkarrusel komponent der dynamisk henter indhold fra wordpress api'et -->
   <!-- Her bruges JavaScript librariet Swiper som er et populært framework til karuseller -->
   <div class="swiper bg-fur-blue">
-
-    <div class="swiper-wrapper md:w-full xl:max-w-[1280px] md:py-[4rem] lg:py-[6rem] py-[2rem]">
+    <div
+      class="swiper-wrapper md:w-full xl:max-w-[1280px] md:py-[4rem] lg:py-[6rem] py-[2rem]"
+    >
       <!-- med v-for loops der over alle de øl der hentes -->
       <div
         class="swiper-slide !flex flex-col md:flex-row items-center justify-center lg:px-[4rem] max-w-screen gap-8"
-
         v-for="beer in beers"
         :key="beer.id"
       >
@@ -17,30 +17,38 @@
           <img
             :src="beer.acf.billede1?.url"
             :alt="beer.acf.billede1?.alt"
-
             class="h-[20rem] md:h-[30rem] object-cover transition-transform duration-300"
-
           />
         </div>
 
         <div class="flex flex-col items-center lg:items-start">
           <!-- her vises navn og introtitel der hentes for hver øl -->
 
-          <h2 class="text-neutral-6 text-center md:text-left">{{ beer.acf.navn }}</h2>
-          <p class="text-neutral-6 p-medium text-center md:text-left">{{ beer.acf.intro_titel }}</p>
+          <h2 class="text-neutral-6 text-center md:text-left">
+            {{ beer.acf.navn }}
+          </h2>
+          <p class="text-neutral-6 p-medium text-center md:text-left">
+            {{ beer.acf.intro_titel }}
+          </p>
 
-          <div class="flex items-center justify-center flex-col text-center lg:flex-row lg:text-left md:mt-[10rem]">
+          <div
+            class="flex items-center justify-center flex-col text-center lg:flex-row lg:text-left md:mt-[10rem]"
+          >
             <!-- Her vises 3 stykker metadata for hver øl -->
             <div class="text-neutral-6 md:w-auto">
               <p class="opacity-50 text-xl">Stilart</p>
               <p class="p-medium">{{ beer.acf.stilart[0]?.name }}</p>
             </div>
-            <span class="w-1/2 h-px bg-fur-accent-beer my-4 lg:rotate-90 md:w-[8rem] block"></span>
+            <span
+              class="w-1/2 h-px bg-fur-accent-beer my-4 lg:rotate-90 md:w-[8rem] block"
+            ></span>
             <div class="text-neutral-6 md:w-auto">
               <p class="opacity-50 text-xl">Alkoholprocent</p>
               <p class="p-medium">{{ beer.acf.alkoholprocent }}%</p>
             </div>
-            <span class="w-1/2 h-px bg-fur-accent-beer my-4 lg:rotate-90 md:w-[8rem] block"></span>
+            <span
+              class="w-1/2 h-px bg-fur-accent-beer my-4 lg:rotate-90 md:w-[8rem] block"
+            ></span>
             <div class="text-neutral-6 md:w-auto">
               <p class="opacity-50 text-xl">Smagsnoter</p>
               <p class="p-medium max-w-96">{{ beer.acf.smagsnoter }}</p>
@@ -55,7 +63,9 @@
               @mouseenter="growImage"
               @mouseleave="shrinkImage"
             >
-              <span class="text-lg whitespace-nowrap">Smag på {{ beer.acf.navn }}</span>
+              <span class="text-lg whitespace-nowrap"
+                >Smag på {{ beer.acf.navn }}</span
+              >
               <span
                 class="absolute -z-10 left-0 top-0 w-full h-full bg-fur-accent-blue opacity-0 group-hover:opacity-100 group-hover:-left-[6px] group-hover:top-[6px] group-active:-left-[8px] group-active:top-[8px] transition-all duration-200"
               ></span>
@@ -69,18 +79,13 @@
 
     <div class="swiper-button-prev mx-4"></div>
     <div class="swiper-button-next mx-4"></div>
-
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, nextTick } from 'vue';
-import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
+import { ref, onMounted, nextTick } from "vue";
 
-=======// Her defineres en prop hvor man selv skal vælge id'et som man gerne vil sortere på oppe i url'et
+// Her defineres en prop hvor man selv skal vælge id'et som man gerne vil sortere på oppe i url'et
 
 const props = defineProps({
   produktSorteringId: {
@@ -88,7 +93,6 @@ const props = defineProps({
     required: true,
   },
 });
-
 
 // Tomt array til ølene
 // Her importeres Swiper, navigationen og den css som skal bruges for at karrusellen virker funktionelt
@@ -113,7 +117,6 @@ const initSwiper = () => {
   });
 };
 
-
 // Når komponenten er indsat kaldes on denne onmounted.
 // async bruges for at der kan bruges await indeni
 onMounted(async () => {
@@ -125,30 +128,27 @@ onMounted(async () => {
   const data = await response.json();
   beers.value = data;
 
+  // Funktion der skalerer billedet op når der hoveres over knappen
+  const growImage = (e) => {
+    const slide = e.target.closest(".swiper-slide");
+    if (!slide) return;
+    const img = slide.querySelector("img");
+    if (!img) return;
+    img.classList.add("scale-105");
+  };
 
-
-// Funktion der skalerer billedet op når der hoveres over knappen
-const growImage = (e) => {
-  const slide = e.target.closest('.swiper-slide');
-  if (!slide) return;
-  const img = slide.querySelector('img');
-  if (!img) return;
-  img.classList.add('scale-105');
-};
-
-const shrinkImage = (e) => {
-  const slide = e.target.closest('.swiper-slide');
-  if (!slide) return;
-  const img = slide.querySelector('img');
-  if (!img) return;
-  img.classList.remove('scale-105');
-};
+  const shrinkImage = (e) => {
+    const slide = e.target.closest(".swiper-slide");
+    if (!slide) return;
+    const img = slide.querySelector("img");
+    if (!img) return;
+    img.classList.remove("scale-105");
+  };
 
   // nexttick er en funktion i vue, der venter på at dom'en opdaterer. det gøres for at swiper karusellen virker korrekt og at alt data er hentet
   await nextTick();
   initSwiper();
 });
-
 </script>
 
 <style scoped>
